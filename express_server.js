b19c5903d1624ed64e228aa45d4716ba438ef903
Blake Sartin <blake.sartin8@gmail.com>
2022-01-24 21:03:41 +0000

    add ejs

--- express_server.js
+++ express_server.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
-  res.end()
+  res.end();
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-  res.end()
+  res.end();
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
@@ -22,4 +23,3 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
